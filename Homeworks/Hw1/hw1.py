70593d0754b94bcbb55f2a2d878bf7b8d31b47f1
ktinh02 <phanlekimtinh0987@gmail.com>
2024-12-30 10:12:41 +0700

    Update

--- Homeworks/Hw1/hw1.py
+++ Homeworks/Hw1/hw1.py
@@ -14,11 +14,16 @@ def calculate_probability(event: int, trials: int) -> float:
     if event < 0 or event > trials:
         raise ValueError("The number of event occurrences must be between 0 and the total number of trials.")
 
-    probability = event / trials
-    return probability
+    return event/trials
 
+try:
+    event_count = int(input("Enter number of event: "))
+    total_trials = int(input("Enter total number of trials: "))
 
-event_count = 15  # Number of event occurrences
-total_count = 50  # Total number of trials
-result = calculate_probability(event_count, total_count)
-print(f"The probability of the event is: {result:.2f}")
+    probability = calculate_probability(event_count, total_trials)
+    print(f"The probability of the event is: {probability:.2f}")
+
+except ValueError as e:
+    print(f"Error: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
